a543d366844f9a9662c8035caee2da6adad4105c
Jacob Toomey <jtoomey@ucsb.edu>
2023-04-09 14:14:14 -0700

    Update MenuItem.java

--- src/main/java/edu/ucsb/cs56/pconrad/menuitem/MenuItem.java
+++ src/main/java/edu/ucsb/cs56/pconrad/menuitem/MenuItem.java
@@ -43,23 +43,27 @@ public class MenuItem {
         return "$"+priceInCents/100+"."+priceInCents%100;
     }
 
-    /**
-     * Returns the price, formatted as a string with a $,
-     * right justified in a field with the specified width.
-     * For example "$0.99", "$10.99", or "$3.50".
-     * <p>
-     * If the width is too small, throws TooNarrowException
-     *
-     * @param width width of returned string
-     */
+/**
+ * Returns the price, formatted as a string with a $,
+ * right justified in a field with the specified width.
+ * For example "$0.99", "$10.99", or "$3.50".
+ * <p>
+ * If the width is too small, throws TooNarrowException
+ *
+ * @param width width of returned string
+ */
 
-    public String getPrice(int width) {
-        String s="$"+priceInCents/100+"."+priceInCents%100;;
-        if (s.length()>width)
-            {throw new TooNarrowException();}
-        return s;
+ public String getPrice(int width) throws TooNarrowException {
+    String s = String.format("$%1$,.2f", (double) priceInCents / 100);
+    if (s.length() > width) {
+        throw new TooNarrowException();
     }
+    return String.format("%1$" + width + "s", s);
+}
 
+public String  getCategory(){
+    return this.category;
+}
     /**
      * return a string in csv format, in the order name,price,cateogry.
      * For example <code>Small Poke Bowl,1049,Poke Bowls</code>
@@ -68,13 +72,9 @@ public class MenuItem {
      */
 
 
-    public String  getCategory(){
-        return "stub";
-    }
-
     @Override
     public String toString() {
-        return "stub";
+        return name+","+priceInCents+","+category;
     }
 
 }
